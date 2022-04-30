7f0f6d84d16917c2db7bfdd6b4de6acb14250fe7
unknown <tomeryehezqel@gmail.com>
2022-04-30 20:09:35 +0300

    uploding html files works, need to add headers for jpg etc

--- main.py
+++ main.py
@@ -13,44 +13,50 @@ import socket
 LOCALHOST = '127.0.0.1'
 IP = '0.0.0.0'
 PORT = 80
-SOCKET_TIMEOUT = 0.1
+SOCKET_TIMEOUT = 1000
 PROJECT_ROOT_DIR = os.getcwd()
 WEB_ROOT_DIR = os.path.join(PROJECT_ROOT_DIR, 'webroot', 'webroot')
-FIXED_RESPONSE = f'{WEB_ROOT_DIR}\\index.html'
+FIXED_RESPONSE = ''
+DEFAULT_URL = os.path.join(WEB_ROOT_DIR, 'index.html').replace('\\',  '/')
 
 
 def get_file_data(filename):
     """ Get data from file """
-    return
+    filename_path = os.path.join(WEB_ROOT_DIR, filename[1:]).replace('\\',  '/')
+
+    with open(filename_path, "r", encoding='utf-8') as file:
+        file_data = file.read()
+    return file_data
 
 
 def handle_client_request(resource, client_socket):
     """ Check the required resource, generate proper HTTP response and send to client"""
     # TO DO : add code that given a resource (URL and parameters) generates the proper response
-    return
 
-    """
-    if resource == '':
-        url = DEFAULT_URL
+    if resource == '/':
+        url = '/' + DEFAULT_URL.split('/')[-1]
     else:
         url = resource
 
     # TO DO: check if URL had been redirected, not available or other error code. For example:
-    if url in REDIRECTION_DICTIONARY:
-        # TO DO: send 302 redirection response
+    # if url in REDIRECTION_DICTIONARY:
+    # TO DO: send 302 redirection response
 
     # TO DO: extract requested file tupe from URL (html, jpg etc)
+    filetype = url.split('.')[-1]
     if filetype == 'html':
-        http_header = # TO DO: generate proper HTTP header
+        http_header = 'Accept: text/html\r\n'  # TO DO: generate proper HTTP header
+    """
     elif filetype == 'jpg':
-        http_header = # TO DO: generate proper jpg header
+        http_header =  # TO DO: generate proper jpg header
     # TO DO: handle all other headers
+    """
 
     # TO DO: read the data from the file
-    data = get_file_data(filename)
-    http_response = http_header + data
+    data = get_file_data(url)
+    # http_response = http_header + data
+    http_response = data
     client_socket.send(http_response.encode())
-    """
 
 
 def validate_http_request(request):
@@ -74,7 +80,7 @@ def handle_client(client_socket):
         valid_http, resource = validate_http_request(client_request)
         if valid_http:
             print(f'Got a valid HTTP request: {resource}')
-            # handle_client_request(resource, client_socket)
+            handle_client_request(resource, client_socket)
             break
         else:
             print('Error: Not a valid HTTP request')
